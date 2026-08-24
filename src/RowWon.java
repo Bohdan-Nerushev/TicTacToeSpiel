51cf140fbb47b5823d58b0a159c65fb40ebf6165
Bohdan Nerushev <bnerushev@united-internet.de>
2026-08-24 15:50:55 +0200

    refactor: remove redundant comments in rowwon class

--- src/RowWon.java
+++ src/RowWon.java
@@ -3,7 +3,6 @@ public class RowWon {
     private int winsX = 0;
     private int winsO = 0;
 
-    // Додати перемогу гравцю
     public void addWin(char player) {
         if (player == 'X') {
             winsX++;
@@ -12,19 +11,16 @@ public class RowWon {
         }
     }
 
-    // Отримати кількість перемог
     public int getWins(char player) {
         if (player == 'X') return winsX;
         if (player == 'O') return winsO;
         return 0;
     }
 
-    // Перевірити чи гравець досяг 3 перемог
     public boolean hasTripleWin(char player) {
         return getWins(player) >= 3;
     }
 
-    // Скинути статистику (нова гра)
     public void reset() {
         winsX = 0;
         winsO = 0;
